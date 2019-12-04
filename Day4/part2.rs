a27373849db048d01a1b07388bc66899d3bfc31d
Nikolas De Giorgis <bznein@gmail.com>
2019-12-04 08:35:36 +0000

    Update part2.rs

--- Day4/part2.rs
+++ Day4/part2.rs
@@ -1,7 +1,6 @@
-use std::process;
+use std::cmp::Ordering;
 
-fn num_to_vec(mut input : u32) -> Vec<u32>
-{
+fn num_to_vec(mut input: u32) -> Vec<u32> {
     let n = (input as f32).log10() as usize + 1;
     let mut buf = Vec::with_capacity(n);
     while input != 0 {
@@ -12,38 +11,32 @@ fn num_to_vec(mut input : u32) -> Vec<u32>
     buf
 }
 
-fn is_valid_password(num : u32) -> bool
-{
+fn is_valid_password(num: u32) -> bool {
     let vec = num_to_vec(num);
     let mut double_digit = false;
-    for i in 1..vec.len()
-    {
-        if vec[i]<vec[i-1]
-        {
-            return false;
-        }
-        else if vec[i] == vec[i-1]
-        {
-            if (i<2|| vec[i-2]!=vec[i]) && (i+1>=vec.len() || vec[i+1]!=vec[i])
-            {
-                double_digit=true;
+    for i in 1..vec.len() {
+        match vec[i].cmp(&vec[i - 1]) {
+            Ordering::Less => return false,
+            Ordering::Equal => {
+                if (i < 2 || vec[i - 2] != vec[i]) && (i + 1 >= vec.len() || vec[i + 1] != vec[i]) {
+                    double_digit = true;
+                }
             }
+            _ => (),
         }
     }
     double_digit
 }
 
 fn main() {
-    let lower : u32 = 367_479;
-    let higher : u32 = 893_698;
+    let lower: u32 = 367_479;
+    let higher: u32 = 893_698;
     let mut count = 0;
-    for num in lower..=higher
-    {
-        if is_valid_password(num)
-        {
-            count +=1;
+    for num in lower..=higher {
+        if is_valid_password(num) {
+            count += 1;
         }
     }
-    
+
     println!("{}", count);
 }
