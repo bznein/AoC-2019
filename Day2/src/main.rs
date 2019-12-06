0aadee03bb8b2a0d6721a00058c24ec56ed996a5
nikolas <nikolas.degiorgis>
2019-12-06 20:40:46 +0100

    Added stdin input to days 2-7

--- Day2/src/main.rs
+++ Day2/src/main.rs
@@ -1,4 +1,5 @@
 use std::convert::TryInto;
+use std::io::{self, Read};
 use std::process;
 
 fn get_final_value(noun: i32, verb: i32, mut v: Vec<i32>) -> i32 {
@@ -20,20 +21,22 @@ fn get_final_value(noun: i32, verb: i32, mut v: Vec<i32>) -> i32 {
 }
 
 fn main() {
-    let input = String::from("1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,6,27,1,9,27,31,1,31,10,35,2,13,35,39,1,39,10,43,1,43,9,47,1,47,13,51,1,51,13,55,2,55,6,59,1,59,5,63,2,10,63,67,1,67,9,71,1,71,13,75,1,6,75,79,1,10,79,83,2,9,83,87,1,87,5,91,2,91,9,95,1,6,95,99,1,99,5,103,2,103,10,107,1,107,6,111,2,9,111,115,2,9,115,119,2,13,119,123,1,123,9,127,1,5,127,131,1,131,2,135,1,135,6,0,99,2,0,14,0");
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    if input.ends_with('\n')
+    {
+        input.truncate(input.len()-1);
+    }
     let v: Vec<i32> = input
         .split(',')
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
-    
 
+    println!("Part 1: {:}", get_final_value(12, 2, v.clone()));
+    let output: i32 = 19_690_720;
 
-
-    println!("Part 1: {:}", get_final_value(12,2,v.clone()));
-	
-	let output: i32 = 19_690_720;
-
-	for i in 0..=99 {
+    for i in 0..=99 {
         for j in 0..=99 {
             if get_final_value(i, j, v.clone()) == output {
                 println!("Part 2: {}", 100 * i + j);
