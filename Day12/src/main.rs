a7a5f33d20e848d3e18f3d6de6fedc54d9a4c75f
nikolas <nikolas.degiorgis>
2019-12-13 22:28:04 +0100

    refactor of D12

--- Day12/src/main.rs
+++ Day12/src/main.rs
@@ -2,38 +2,28 @@ use itertools::Itertools;
 use std::collections::HashSet;
 use num::integer::lcm;
 
-#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
-struct P
-{
- x: i64,
- y: i64,
- z: i64,
-}
 
-#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
-struct V
-{
- x: i64,
- y: i64,
- z: i64,
-}
-
-#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
 struct Moon
 {
-	p: P,
-	v: V,
+	p: Vec<i64>,
+	v: Vec<i64>,
 }
 
 impl Moon{
 
-	pub fn new(p : P) -> Moon
+	pub fn new(p: Vec<i64>) -> Moon
 	{
 		Moon {
 			p : p,
-			v : V { x: 0, y:0, z:0 },
+			v : vec![0,0,0],
 		}
 	}
+
+    pub fn is_equal_dir(&self, m : &Moon, dir: usize) -> bool
+    {
+        self.p[dir] == m.p[dir] && self.v[dir] == m.v[dir]
+    }
 
 }
 
@@ -42,8 +32,8 @@ fn total_energy(moons: &Vec<Moon>) -> i64
 	let mut total = 0;
 	for m in moons 
 	{
-		let potential = (m.p.x.abs()+m.p.y.abs()+m.p.z.abs());
-		let kinetic = (m.v.x.abs()+m.v.y.abs()+m.v.z.abs());
+		let potential = (m.p[0].abs()+m.p[1].abs()+m.p[2].abs());
+		let kinetic = (m.v[0].abs()+m.v[1].abs()+m.v[2].abs());
 		total += potential * kinetic;
 	}
 	total
@@ -53,14 +43,8 @@ fn is_equal_dir(moon1 : &Vec<Moon>, moon2 : &Vec<Moon>, dir: usize) -> bool
 {
 	let mut result = true;
 	for i in 0..moon1.len()
-	{
-			match dir
-			{
-				0=> result = result && moon1[i].p.x == moon2[i].p.x && moon1[i].v.x == moon2[i].v.x,
-				1=> result = result && moon1[i].p.y == moon2[i].p.y && moon1[i].v.y == moon2[i].v.y,
-				2=> result = result && moon1[i].p.z == moon2[i].p.z && moon1[i].v.z == moon2[i].v.z,
-				_ => panic!("Wrong direction"),
-			}
+	  {
+        result = result && moon1[i].is_equal_dir(&moon2[i],dir);
 	}
 	result
 }
@@ -92,59 +76,21 @@ fn step_moons(moons: & mut Vec<Moon>, dir: usize)
 		{
 			for j in i+1..4
 			{	
-				match dir 
-				{
-					0 => {
-						if (moons[i].p.x<moons[j].p.x)
+						if (moons[i].p[dir]<moons[j].p[dir])
 						{
-							moons[i].v.x+=1;
-							moons[j].v.x-=1;
+							moons[i].v[dir]+=1;
+							moons[j].v[dir]-=1;
 						}
-						else if (moons[i].p.x>moons[j].p.x)
+						else if (moons[i].p[dir]>moons[j].p[dir])
 						{
-							moons[i].v.x-=1;
-							moons[j].v.x+=1;
-						}
-					}
-					1 =>
-					{
-						if (moons[i].p.y<moons[j].p.y)
-						{
-							moons[i].v.y+=1;
-							moons[j].v.y-=1;
-						}
-						else if (moons[i].p.y>moons[j].p.y)
-						{
-							moons[i].v.y-=1;
-							moons[j].v.y+=1;
-						}
-					}
-					2 =>
-					{
-						if (moons[i].p.z<moons[j].p.z)
-						{
-							moons[i].v.z+=1;
-							moons[j].v.z-=1;
-						}
-						else if (moons[i].p.z>moons[j].p.z)
-						{
-							moons[i].v.z-=1;
-							moons[j].v.z+=1;
-						}
-					}
-					_ => panic!("Wrong direction"),
-				}
+							moons[i].v[dir]-=1;
+							moons[j].v[dir]+=1;
+						}		
 			}
 		}
 		for i in 0..4
 		{
-			match dir
-			{
-				0=> moons[i].p.x+=moons[i].v.x,
-				1=> moons[i].p.y+=moons[i].v.y,
-				2=> moons[i].p.z+=moons[i].v.z,
-				_ => panic!("Wrong direction"),
-			}
+        moons[i].p[dir]+=moons[i].v[dir];
 		}	
 }
 
@@ -152,10 +98,10 @@ fn step_moons(moons: & mut Vec<Moon>, dir: usize)
 do not care about parsing it, it's just not worth it */
 fn main() {
 	let mut moons = Vec::new();
-	moons.push(Moon::new(P{ x: -6, y:-5, z: -8}));
-	moons.push(Moon::new(P{ x: 0, y:-3, z: -13}));
-	moons.push(Moon::new(P{ x: -15, y:10, z: -11}));
-	moons.push(Moon::new(P{ x: -3, y:-8, z: 3}));
+	moons.push(Moon::new(vec![-6,-5,-8]));
+	moons.push(Moon::new(vec![0,-3,-13]));
+	moons.push(Moon::new(vec![-15, 10,-11]));
+	moons.push(Moon::new(vec![-3,-8,3]));
 	
 	let period = universe_period(&moons.clone());
 	
