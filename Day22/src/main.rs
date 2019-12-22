c574d67e0f179e0c5ef9ad322e5857259e8540b6
nikolas <nikolas.degiorgis>
2019-12-22 19:10:57 +0100

    D22 p2

--- Day22/src/main.rs
+++ Day22/src/main.rs
@@ -1,72 +1,9 @@
 use std::io::{self, Read};
-
-fn deal(deck: &mut Vec<u16>)
-{
-    deck.reverse()
-}
-
-fn cut_front(deck: &mut Vec<u16>, n: usize)
-{
-    let num_cards = deck.len();
-    let mut buf = Vec::new();
-    for i in 0..n
-    {
-        buf.push(deck[i]);
-    }
-    for i in 0..num_cards-n
-    {
-        deck[i]=deck[i+n];
-    }
-    for i in 0..n
-    {
-        deck[num_cards-n+i]=buf[i];
-    }
-}
-
-fn cut_back(deck: &mut Vec<u16>, n: usize)
-{
-    let num_cards = deck.len();
-    let mut buf = Vec::new();
-    for i in 0..n
-    {
-        buf.push(deck[num_cards-1-i]);
-    }
-    for i in 0..num_cards-n
-    {
-        deck[num_cards-1-i]=deck[num_cards-1-i-n];
-    }
-    for i in 0..n
-    {
-        deck[i]=buf[n-1-i];
-    }
-}
-
-fn deal_with_increment(deck: &Vec<u16>, increment: usize) -> Vec<u16>
-{
-    let num_cards = deck.len();
-    let mut new_deck = vec![0;num_cards];
-    for i in 0..num_cards
-    {
-        new_deck[(increment*i)%num_cards]=deck[i]
-    }
-    new_deck
-}
-
-fn cut(deck: &mut Vec<u16>, n: i32)
-{
-    if n > 0
-    {
-        cut_front(deck, n as usize);
-    }
-    else
-    {
-        cut_back(deck, (-n) as usize);
-    }
-}
+use std::collections::HashMap;
+use modinverse::modinverse;
+use mod_exp::mod_exp;
 
 fn main() {
-    let num_cards = 10007;
-    let mut deck : Vec<_> = (0u16..num_cards).map(u16::from).collect();
     let mut input = String::new();
 
     io::stdin().read_to_string(&mut input).unwrap();
@@ -75,32 +12,96 @@ fn main() {
         input.truncate(input.len() - 1);
     }
 
-    for command in input.split('\n')
+    let params : Vec<_>= input.split('\n').map(|x| x.split_whitespace().collect::<Vec<_>>()).collect();
+
+    let cmd : Vec<(usize,i128)>=
+        params
+        .iter()
+        .map(|x| if x[0]=="deal"
+             {
+                 if x[1] == "into"
+                 {
+                     (0,-1)
+                 }
+                 else
+                 {
+                     (1,x[3].parse::<i128>().unwrap())
+                 }
+             }
+             else
+             {
+                 (2,x[1].parse::<i128>().unwrap())
+             }
+        )
+        .collect();
+
+
+    let num_cards : i128 = 10007;
+    let mut index : i128 = 2019;
+
+    for param in &cmd
     {
-        let param : Vec<_> = command.split_whitespace().collect();
-        if param[0]=="deal"
+        index = match param.0
         {
-            if param[1] == "into"
-            {
-                deal(& mut deck);
-            }
-            else if param[1] == "with"
-            {
-                deck = deal_with_increment(&deck, param[3].parse::<usize>().unwrap());
-            }
-            else
-            {
-                panic!("Unknown command! : {:?}", param);
-            }
-        }
-        else if param[0] =="cut"
-        {
-            cut(&mut deck, param[1].parse::<i32>().unwrap());
-        }
-        else
-        {
-            panic!("Unknown command! : {:?}", param);
+            0 => num_cards-1-index,
+            1 => (index*param.1)%num_cards,
+            2 => (index-param.1+num_cards)%num_cards,
+            _ => panic!("Unknown command!"),
         }
     }
-    println!("Part 1: {}", deck.iter().position(|&x| x==2019).unwrap());
+
+    println!("Part 1: {}", index);
+
+
+
+    let num_cards : i128 = 119315717514047;
+    let index : i128 = 2020;
+    let num_shuffles : i128 = 101741582076661;
+
+    let mut a = 1;
+    let mut b = 0;
+    for param in cmd.iter().rev()
+    {
+        match param.0
+        {
+            0 => {
+                a*=-1;
+                b+=1;
+                b*=-1;
+            }
+            1 =>
+            {
+                let inv = modinverse(param.1,num_cards).unwrap();
+                a = a * inv % num_cards;
+                b = b * inv % num_cards;
+            }
+            2 =>
+            {
+                b += if param.1 < 0 {param.1 + num_cards} else {param.1};
+            }
+            _ => panic!("Unknown command!"),
+        }
+        a %= num_cards;
+        b %= num_cards;
+
+        if a < 0 {
+            a += num_cards;
+        }
+
+        if b < 0 {
+            b += num_cards;
+        }
+    }
+
+    let a_n = mod_exp(a,num_shuffles,num_cards);
+    if a==1
+    {
+        println!("Part 2: {}", a_n*index+b*num_shuffles);
+        return;
+    }
+    let a_n1 = (a_n-1)%num_cards;
+    let inv = modinverse(a-1,num_cards).unwrap();
+    println!("Part 2: {}", (index*a_n + ((b * a_n1)%num_cards) * inv)%num_cards);
+
+
 }
