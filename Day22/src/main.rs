8a51bdce0e0d65773ae61d2f93d25318b9b36ad9
nikolas <nikolas.degiorgis>
2019-12-23 18:30:37 +0100

    d23

--- Day22/src/main.rs
+++ Day22/src/main.rs
@@ -1,7 +1,6 @@
-use std::io::{self, Read};
-use std::collections::HashMap;
-use modinverse::modinverse;
 use mod_exp::mod_exp;
+use modinverse::modinverse;
+use std::io::{self, Read};
 
 fn main() {
     let mut input = String::new();
@@ -12,72 +11,64 @@ fn main() {
         input.truncate(input.len() - 1);
     }
 
-    let params : Vec<_>= input.split('\n').map(|x| x.split_whitespace().collect::<Vec<_>>()).collect();
-
-    let cmd : Vec<(usize,i128)>=
-        params
-        .iter()
-        .map(|x| if x[0]=="deal"
-             {
-                 if x[1] == "into"
-                 {
-                     (0,-1)
-                 }
-                 else
-                 {
-                     (1,x[3].parse::<i128>().unwrap())
-                 }
-             }
-             else
-             {
-                 (2,x[1].parse::<i128>().unwrap())
-             }
-        )
+    let params: Vec<_> = input
+        .split('\n')
+        .map(|x| x.split_whitespace().collect::<Vec<_>>())
         .collect();
 
+    let cmd: Vec<(usize, i128)> = params
+        .iter()
+        .map(|x| {
+            if x[0] == "deal" {
+                if x[1] == "into" {
+                    (0, -1)
+                } else {
+                    (1, x[3].parse::<i128>().unwrap())
+                }
+            } else {
+                (2, x[1].parse::<i128>().unwrap())
+            }
+        })
+        .collect();
 
-    let num_cards : i128 = 10007;
-    let mut index : i128 = 2019;
+    let num_cards: i128 = 10007;
+    let mut index: i128 = 2019;
 
-    for param in &cmd
-    {
-        index = match param.0
-        {
-            0 => num_cards-1-index,
-            1 => (index*param.1)%num_cards,
-            2 => (index-param.1+num_cards)%num_cards,
+    for param in &cmd {
+        index = match param.0 {
+            0 => num_cards - 1 - index,
+            1 => (index * param.1) % num_cards,
+            2 => (index - param.1 + num_cards) % num_cards,
             _ => panic!("Unknown command!"),
         }
     }
 
     println!("Part 1: {}", index);
 
-
-
-    let num_cards : i128 = 119315717514047;
-    let index : i128 = 2020;
-    let num_shuffles : i128 = 101741582076661;
+    let num_cards: i128 = 119_315_717_514_047;
+    let index: i128 = 2020;
+    let num_shuffles: i128 = 101_741_582_076_661;
 
     let mut a = 1;
     let mut b = 0;
-    for param in cmd.iter().rev()
-    {
-        match param.0
-        {
+    for param in cmd.iter().rev() {
+        match param.0 {
             0 => {
-                a*=-1;
-                b+=1;
-                b*=-1;
+                a *= -1;
+                b += 1;
+                b *= -1;
             }
-            1 =>
-            {
-                let inv = modinverse(param.1,num_cards).unwrap();
+            1 => {
+                let inv = modinverse(param.1, num_cards).unwrap();
                 a = a * inv % num_cards;
                 b = b * inv % num_cards;
             }
-            2 =>
-            {
-                b += if param.1 < 0 {param.1 + num_cards} else {param.1};
+            2 => {
+                b += if param.1 < 0 {
+                    param.1 + num_cards
+                } else {
+                    param.1
+                };
             }
             _ => panic!("Unknown command!"),
         }
@@ -93,15 +84,15 @@ fn main() {
         }
     }
 
-    let a_n = mod_exp(a,num_shuffles,num_cards);
-    if a==1
-    {
-        println!("Part 2: {}", a_n*index+b*num_shuffles);
+    let a_n = mod_exp(a, num_shuffles, num_cards);
+    if a == 1 {
+        println!("Part 2: {}", a_n * index + b * num_shuffles);
         return;
     }
-    let a_n1 = (a_n-1)%num_cards;
-    let inv = modinverse(a-1,num_cards).unwrap();
-    println!("Part 2: {}", (index*a_n + ((b * a_n1)%num_cards) * inv)%num_cards);
-
-
+    let a_n1 = (a_n - 1) % num_cards;
+    let inv = modinverse(a - 1, num_cards).unwrap();
+    println!(
+        "Part 2: {}",
+        (index * a_n + ((b * a_n1) % num_cards) * inv) % num_cards
+    );
 }
---
+++ Day23/src/main.rs
@@ -0,0 +1,117 @@
+use intcode::IntcodeMachine;
+use intcode::State;
+use std::collections::VecDeque;
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    if input.ends_with('\n') {
+        input.truncate(input.len() - 1);
+    }
+
+    let v: Vec<i64> = input
+        .split(',')
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect();
+
+    let num_machines = 50;
+    let mut input_queues = vec![VecDeque::new(); num_machines];
+    let mut executors = vec![IntcodeMachine::new(v.clone()); num_machines];
+    for i in 0..num_machines {
+        executors[i].set_input(i as i64);
+    }
+    let mut i = 0;
+
+    loop {
+        executors[i].run();
+        match executors[i].state() {
+            State::WaitingForInput => {
+                executors[i].set_input(if let Some(x) = input_queues[i].pop_front() {
+                    x
+                } else {
+                    -1
+                });
+            }
+            State::Stopped => {
+                let val = executors[i].get_output().unwrap();
+                executors[i].run();
+                let x = executors[i].get_output().unwrap();
+                if val != 255 {
+                    input_queues[val as usize].push_back(x);
+                }
+                executors[i].run();
+                let y = executors[i].get_output().unwrap();
+                if val == 255 {
+                    println!("Part 1: {}", y);
+                    break;
+                }
+                input_queues[val as usize].push_back(y);
+            }
+            State::Halted => break,
+            State::Running => panic!("Still running"),
+        }
+        i = (i + 1) % num_machines;
+    }
+
+    let mut input_queues = vec![VecDeque::new(); num_machines];
+    let mut executors = vec![IntcodeMachine::new(v.clone()); num_machines];
+    for i in 0..num_machines {
+        executors[i].set_input(i as i64);
+    }
+    let mut i = 0;
+
+    let mut nat: (i64, i64) = (0, 0);
+    let mut last_y_to_0 = -1;
+    let idle_threshold = 2;
+    let mut is_idle = vec![0; num_machines];
+    loop {
+        executors[i].run();
+        match executors[i].state() {
+            State::WaitingForInput => {
+                executors[i].set_input(if let Some(x) = input_queues[i].pop_front() {
+                    is_idle[i] = 0;
+                    x
+                } else {
+                    is_idle[i] += 1;
+                    if is_idle.iter().all(|x| *x >= idle_threshold) {
+                        input_queues[0].push_back(nat.0);
+                        input_queues[0].push_back(nat.1);
+                        if nat.1 == last_y_to_0 {
+                            println!("Part 2: {}", nat.1);
+                            break;
+                        }
+                        last_y_to_0 = nat.1;
+                        for i in &mut is_idle
+                        {
+                            *i=0;
+                        }
+                    }
+                    -1
+                });
+            }
+            State::Stopped => {
+                is_idle[i] = 0;
+                let val = executors[i].get_output().unwrap();
+                executors[i].run();
+                let x = executors[i].get_output().unwrap();
+                if val != 255 {
+                    input_queues[val as usize].push_back(x);
+                } else {
+                    nat.0 = x;
+                }
+                executors[i].run();
+                let y = executors[i].get_output().unwrap();
+                if val == 255 {
+                    nat.1 = y;
+                } else {
+                    input_queues[val as usize].push_back(y);
+                }
+            }
+            State::Halted => break,
+            State::Running => panic!("Still running"),
+        }
+        i = (i + 1) % num_machines;
+    }
+}
